3b7e052ee6d2340254d6e4046a3c4d966b786d0c
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-01 16:26:23 +0300

    layout

--- myphotos/gallery/urls.py
+++ myphotos/gallery/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('', views.gallery, name='gallery'),
     path('photo<str:pk>/', views.viewPhoto, name='photo'),
     path('add', views.addPhoto, name='add'),
+    # path('search', views.searchPhoto, name='search'),
 ]
